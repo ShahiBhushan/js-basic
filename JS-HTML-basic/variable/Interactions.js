5fc5ea510c59aa899f174329235821801842263c
Bhushan Shahi <bhushanshahi42@gmail.com>
2024-01-21 00:06:22 +0545

    Interaction & operators

---
+++ JS-HTML-basic/variable/Interactions.js
@@ -0,0 +1,9 @@
+// write a js program that store the user information about their birth year and calculate age
+
+//Interaction
+//Type casting => converson  of data type from one data to another(String,Number,etc)
+const year = Number(prompt("What is your birth year?"));
+console.log(year);
+//Operator
+const age= 2024-year;
+console.log(age);
