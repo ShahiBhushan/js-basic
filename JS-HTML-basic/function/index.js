4ef616a2b558a4d90e05daa65458d36b7506351a
Bhushan Shahi <bhushanshahi42@gmail.com>
2024-01-22 09:44:30 +0545

    subtraction function

--- JS-HTML-basic/function/index.js
+++ JS-HTML-basic/function/index.js
@@ -21,3 +21,13 @@ function add(a, b, c) {
   console.log(a + b + c);
 }
 add(20, 10, 1);
+//subtraction
+function sub(a,b){
+  const result=a-b;
+  return result;
+}
+const resp=sub(12,10);
+const resp1=sub(15,11);
+const resp2=sub(120,12);
+const resp3=sub(11,2);
+console.log(resp,resp1,resp2,resp3);
